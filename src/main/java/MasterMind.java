cf57a3a2c34fef9d73d27b3f701671ceb9f7d4f8
AlexandraCabreraLituma <43210191+AlexandraCabreraLituma@users.noreply.github.com>
2018-10-02 09:13:25 -0500

    Add files via upload

---
+++ src/main/java/MasterMind.java
@@ -0,0 +1,30 @@
+
+import controllers.*;
+
+
+public class MasterMind {
+
+	private Logic logic;
+
+	public MasterMind() {
+		logic = new Logic();
+	}
+
+	
+	public void play() {
+		OperationController controller;
+		do {
+			controller = logic.getController();
+			if (controller != null){
+				controller.control();
+			}
+		} while (controller != null);
+	}
+
+	public static void main(String[] args) {
+		new MasterMind().play();
+	}
+	
+	
+	
+}
